febda48a1c4518e9ea7e9c6d3379ec02d32bc5aa
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-11-23 14:39:06 +0100

    canary test 10

--- files/arma3.sh
+++ files/arma3.sh
@@ -9,13 +9,34 @@ runcommand() {
 }
 
 fixnotlowercase() {
-    for mod in $(ls | grep "^@"); do
-        cd "$mod"
-        find . -type f | while read -r file; do
-            mv "$file" "$(echo $file | tr '[:upper:]' '[:lower:]')"
-        done
-        cd ..
+scanfiles=y
+scandirs=y
+mods=$(ls | grep "^@")
+IFS=$'\n' # Set the internal field separator to line break
+for mod in $mods; do
+  cd "$mod"
+  echo convert "$mod"
+  echo change dir to $(pwd)
+  if [ "$scanfiles" = "y" ]; then
+    find ./ -type f | while read -r file; do
+        echo convert file "$file" of mod "$mod"
+        new_name=$(echo "$file" | tr 'A-Z' 'a-z')
+        if [ "$file" != "$new_name" ]; then
+          mv "$file" "$new_name"
+        fi
     done
+  fi
+  if [ "$scandirs" = "y" ]; then
+    find ./ -type d | while read -r dir; do
+        echo convert dir "$dir" of mod "$mod"
+        new_name=$(echo "$dir" | tr 'A-Z' 'a-z')
+        if [ "$dir" != "$new_name" ]; then
+          mv "$dir" "$new_name"
+        fi
+    done
+  fi
+  cd ..
+done
 }
 
 # change workdir
