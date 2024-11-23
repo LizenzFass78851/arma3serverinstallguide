a7074df0bedce891c1d1ec2365691d4484ede8c9
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-11-23 13:18:13 +0100

    canary test 1

--- files/arma3.sh
+++ files/arma3.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
-arma3folder='/srv/steamlibrary/steamapps/common/arma3'
-arma3exe=arma3server_x64
-servercfg=server.cfg
 
-function runcommand() {
-        ./$arma3exe -config=$servercfg -cpuCount=$(nproc) \
-        -mod=$(ls | grep "@" | tr "\n" ";")
+arma3folder='/srv/steamlibrary/steamapps/common/arma3'
+arma3exe='arma3server_x64'
+servercfg='server.cfg'
+
+runcommand() {
+    ./$arma3exe -config=$servercfg -cpuCount=$(nproc) -mod=$(ls | grep "@" | tr "\n" ";")
 }
 
-function fixnotlowercase() {
-        MODS="$(ls | grep "@")"
-        for MOD in ${MODS}; do
-        cd $MOD
-                for file in ./*/*; do mv "$file" "$(echo $file | tr '[:upper:]' '[:lower:]')";
-                done
-        cd ..
+fixnotlowercase() {
+    for mod in $(ls | grep "@"); do
+        cd $mod
+        find . -type f | while read -r file; do
+            mv "$file" "$(echo $file | tr '[:upper:]' '[:lower:]')"
         done
+        cd ..
+    done
 }
 
 # change workdir
 cd $arma3folder
 
 # execute Arma3 server
-echo Starting Arma3 Server
+echo "Starting Arma3 Server"
 runcommand
 
 # failsafe loop
-for (( ; ; ))
-do
-   echo Arma3 Server is down
-   sleep 10s
+while true; do
+    echo "Arma3 Server is down"
+    sleep 10s
 
-   echo Starting Arma3 Server again
+    echo "Starting Arma3 Server again"
 
-   # fix not lowercase problem
-   fixnotlowercase
+    # fix not lowercase problem
+    fixnotlowercase
 
-   # execute Arma3 server again
-   runcommand
+    # execute Arma3 server again
+    runcommand
 done
